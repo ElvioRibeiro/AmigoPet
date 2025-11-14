a4928e00b9437a0095481985ec3b349c8d18fad7
ElvioRibeiro <elvioribeiro@msn.com>
2025-11-14 18:47:08 -0300

    fix(lint): remove debug log to avoid ESLint overlay

--- src/components/NotificationMenu.vue
+++ src/components/NotificationMenu.vue
@@ -95,13 +95,11 @@ export default {
       this.notificationPermission = 'unsupported'
     }
 
-    // Debug: log mount and permission state to help diagnose missing button
-    try {
-      // eslint-disable-next-line no-console
-      console.log('[NotificationMenu] mounted, permission=', this.notificationPermission)
-    } catch (e) {}
+    // Debug log removed to satisfy linting rules. If you need debug output,
+    // re-enable a guarded log during development.
 
     this.fetchNotifications()
+
     // poll every 2 minutes
     this.pollInterval = setInterval(() => this.fetchNotifications(), 2 * 60 * 1000)
   },
